8910d0adb2cb00a30db1ff511a6790466cf98a7f
franpog859 <franciszek.pog@gmail.com>
2021-04-04 13:01:34 +0200

    Add init state to Filter screen

--- aei_map_mobile/lib/features/filter_screen/screens/filter_screen.dart
+++ aei_map_mobile/lib/features/filter_screen/screens/filter_screen.dart
@@ -1,6 +1,21 @@
+import 'package:aei_map_mobile/features/filter_screen/bloc/filter_bloc.dart';
 import 'package:flutter/material.dart';
 
-class FilterScreen extends StatelessWidget {
+class FilterScreen extends StatefulWidget {
+  @override
+  _FilterScreenState createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  final FilterBloc _bloc = FilterBloc();
+
+  @override
+  void initState() {
+    super.initState();
+    _bloc.getFiltres();
+    // TODO: Halp :')
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
